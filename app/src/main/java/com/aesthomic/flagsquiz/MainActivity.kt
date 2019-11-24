142d81739abd536528417f440b1bf7880a1cd832
thomyfarhan <thomyfarhan@gmail.com>
2019-11-24 23:14:03 +0700

    Main - Implement Binding

--- app/src/main/java/com/aesthomic/flagsquiz/MainActivity.kt
+++ app/src/main/java/com/aesthomic/flagsquiz/MainActivity.kt
@@ -2,11 +2,13 @@ package com.aesthomic.flagsquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.aesthomic.flagsquiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
