fe0d5b69fcdf547377c42fb6f1f4782a8b4c251c
Thomy <thomyfarhan@gmail.com>
2019-11-26 21:02:08 +0700

    Game Over Fragment - Create new Fragment

---
+++ app/src/main/java/com/aesthomic/flagsquiz/GameOverFragment.kt
@@ -0,0 +1,20 @@
+package com.aesthomic.flagsquiz
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class GameOverFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_game_over, container, false)
+    }
+
+
+}
