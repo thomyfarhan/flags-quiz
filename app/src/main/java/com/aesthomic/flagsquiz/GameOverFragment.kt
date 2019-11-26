bd0a6a583590e9029101b1509bc916894ad27e4d
Thomy <thomyfarhan@gmail.com>
2019-11-26 21:05:38 +0700

    Game Over Fragment - Change layout & Implement binding

--- app/src/main/java/com/aesthomic/flagsquiz/GameOverFragment.kt
+++ app/src/main/java/com/aesthomic/flagsquiz/GameOverFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.aesthomic.flagsquiz.databinding.FragmentGameOverBinding
 
 class GameOverFragment : Fragment() {
 
@@ -13,8 +15,9 @@ class GameOverFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_game_over, container, false)
+        val binding = DataBindingUtil.inflate<FragmentGameOverBinding>(inflater,
+            R.layout.fragment_game_over, container, false)
+        return binding.root
     }
-
-
+    
 }
