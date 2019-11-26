eb922281a6bc4251ba289d01fa6f65e1ff006e6e
Thomy <thomyfarhan@gmail.com>
2019-11-26 20:35:17 +0700

    Game Won Fragment - Change layout & Implement binding

--- app/src/main/java/com/aesthomic/flagsquiz/GameWonFragment.kt
+++ app/src/main/java/com/aesthomic/flagsquiz/GameWonFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.aesthomic.flagsquiz.databinding.FragmentGameWonBinding
 
 class GameWonFragment : Fragment() {
 
@@ -13,8 +15,9 @@ class GameWonFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_game_won, container, false)
+        val binding = DataBindingUtil.inflate<FragmentGameWonBinding>(inflater,
+            R.layout.fragment_game_won, container, false)
+        return binding.root
     }
-
-
+    
 }
