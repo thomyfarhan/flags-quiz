8cbaec99e3419f57ed6457fe3abf19b06095bafe
Thomy <thomyfarhan@gmail.com>
2019-11-28 19:18:45 +0700

    Main - Add an Up Button in action bar

--- app/src/main/java/com/aesthomic/flagsquiz/MainActivity.kt
+++ app/src/main/java/com/aesthomic/flagsquiz/MainActivity.kt
@@ -3,12 +3,24 @@ package com.aesthomic.flagsquiz
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.aesthomic.flagsquiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        navController = this.findNavController(R.id.quizNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
