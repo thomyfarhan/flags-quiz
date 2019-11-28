69064b856914f1d735f5c18549f9136976cf5d9a
Thomy <thomyfarhan@gmail.com>
2019-11-28 21:45:36 +0700

    Title Fragment - Add menu onClick Handler

--- app/src/main/java/com/aesthomic/flagsquiz/TitleFragment.kt
+++ app/src/main/java/com/aesthomic/flagsquiz/TitleFragment.kt
@@ -2,12 +2,11 @@ package com.aesthomic.flagsquiz
 
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.aesthomic.flagsquiz.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -19,11 +18,21 @@ class TitleFragment : Fragment() {
         val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
             R.layout.fragment_title, container, false)
 
+        setHasOptionsMenu(true)
+
         binding.btnTitleStart.setOnClickListener { v: View ->
             v.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
         }
 
         return binding.root
     }
-    
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, view!!.findNavController()) || super.onOptionsItemSelected(item)
+    }
 }
